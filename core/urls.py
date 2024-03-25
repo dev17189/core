8079d31b7c02b77caef05a4b1f9f7cd5dfe28248
dev17189 <devanshvashistht29@gmail.com>
2024-03-25 23:26:50 +0530

    Added the update functionality for receipes
    also added the user model for further process

--- core/urls.py
+++ core/urls.py
@@ -19,13 +19,23 @@ from django.urls import include, path
 from hom.views import *
 from home.views import *
 from vege.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('', home, name="home"),
+    path('home', home, name="home"),
     path('index',hom, name="index"),
     path('contact',contact, name="contact"),
     path('delete_rec/<id>/',delete_rec, name="delete_rec"),
+    path('update_rec/<id>/',update_rec, name="update_rec"),
     path('about',about, name="about"),
     path('admin/', admin.site.urls),
-    path('receipes',receipes, name="receipes"),
+    path('',receipes, name="receipes"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
--- vege/views.py
+++ vege/views.py
@@ -15,9 +15,33 @@ def receipes(request):
 
 
     queryset=Receipe.objects.all()
+
+    if request.GET.get('search'):
+        queryset=queryset.filter(receipe_name__icontains=request.GET.get('search'))
     context={'Receipes':queryset}
 
     return render(request,'index.html',context)
+
+def update_rec(request,id):
+    query=Receipe.objects.get(id=id)
+    if request.method == 'POST':
+        data=request.POST
+        receipe_img=request.FILES['receipe_image']
+        receipr_name=data.get('receipe_name')
+        receipr_description=data.get('receipe_description')
+
+        query.receipe_name=receipr_name
+        query.receipe_description=receipr_description
+        
+        if receipe_img:
+            query.receipe_image=receipe_img
+        
+        query.save()
+    
+        return redirect('/receipe')
+
+    context={'receipe':query}
+    return render(request,'update_rec.html',context)
 
 def delete_rec(request,id):
     query=Receipe.objects.get(id=id)
